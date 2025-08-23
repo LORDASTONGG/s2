0322c0d6a057971d94178184288d131f2f9a5e0d
LORDASTONG <103436261+LORDASTONGG@users.noreply.github.com>
2025-08-24 01:28:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -193,6 +193,14 @@ function initializeAuth() {
             document.getElementById('login-modal').classList.add('hidden');
             render();
         } 
+        if (nickname === 'fe' && password === 'fe2121') {
+            isAdmin = true;
+            editMode = true;
+            editModeToggle.checked = true;
+            updateAuthUI();
+            document.getElementById('login-modal').classList.add('hidden');
+            render();
+        } 
         
     });
 
